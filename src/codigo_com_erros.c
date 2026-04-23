7b79417c43150ee4f2f5cc307ba1c56ab79a64a7
guivinciguerra <guivinciguerra@gmail.com>
2026-04-23 20:19:01 -0300

    Consertei a formula da media

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -1,15 +1,15 @@
 #include <stdio.h> //"Tirar aspas e incluir <>"
 
 int main() {
-    float nota1, nota2, media
+    float nota1, nota2, media; //"adicionei ; no final da linha"
 
     printf("Digite a primeira nota: ");
     scanf("%f", &nota1);
 
     printf("Digite a segunda nota: ");
-    scanf("%f", nota2);
+    scanf("%f", &nota2); //"adicionei & antes de nota2"
 
-    media = nota1 + nota2 / 2;
+    media = (nota1 + nota2) / 2; //"Consertei a fórmula de cálculo da média"
 
     if (media = 6) {
         printf("Aluno aprovado\n");

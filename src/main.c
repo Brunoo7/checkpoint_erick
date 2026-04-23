d4097852f856cbfbacd534ec34dd04a308fda77a
gabrielgoes08dev <gabriel.goes.08.dev@gmail.com>
2026-04-23 19:12:21 -0300

    iniciando o swit case

--- src/main.c
+++ src/main.c
@@ -7,7 +7,18 @@ int main() {
         printf("\n==============================\n");
 }
     
+do {
+        menu();
+        printf("Escolha: ");
+        scanf("%d", &opcao);
 
+        switch (opcao) {
+
+        case 1:
+            printf("Nota 1: ");
+            scanf("%f", &nota1);
+            printf("Nota 2: ");
+            scanf("%f", &nota2);
     
     return 0;
 }
